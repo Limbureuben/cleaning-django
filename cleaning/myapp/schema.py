56f8bac53074abff8be6dd7488bacdf85691ecf3
Limbureuben <limbureuben4@gmail.com>
2025-05-26 19:58:57 +0300

    changes

--- cleaning/myapp/schema.py
+++ cleaning/myapp/schema.py
@@ -1,10 +1,14 @@
 import graphene
 from .views import *
 
+class Mutation(graphene.ObjectType):
+    register_user = RegisterUser.Field()
+    login_user = LoginUser.Field()
+
 class Query(graphene.ObjectType):
     hello = graphene.String(name=graphene.String(default_value="stanger"))
 
     def resolve_hello(self, info, name):
         return f"Hello, {name}!"
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
