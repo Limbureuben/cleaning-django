5889d14e5661c7334fcc19290b5138ad3f204303
Limbureuben <limbureuben4@gmail.com>
2025-05-26 13:58:40 +0300

    changes

---
+++ cleaning/myapp/schemas.py
@@ -0,0 +1,7 @@
+import graphene
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(name=graphene.String(default_value="stranger"))
+
+    def resolve_hello(self, info, name):
+        return f"Hello, {name}!"
