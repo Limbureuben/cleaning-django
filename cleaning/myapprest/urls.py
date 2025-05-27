3e3de79efa5f107536e6f46b238d5ecc3c53596d
janeth phimbo <emramadhani12@gmail.com>
2025-05-28 01:38:16 +0300

    UPDATE:Adding CRUD OPERATION FOR REVIEWS AND EMPLOYEES

--- cleaning/myapprest/urls.py
+++ cleaning/myapprest/urls.py
@@ -8,5 +8,19 @@ urlpatterns = [
     path('companies/create/', views.create_company, name='company-create'),
     path('companies/<uuid:company_id>/update/', views.update_company, name='company-update'),
     path('companies/<uuid:company_id>/delete/', views.delete_company, name='company-delete'),
+    path('companies/<uuid:company_id>/reviews/', views.company_reviews, name='company-reviews'),
+    # This endpoint url pointd for review CRUD operations
+    path('reviews/', views.review_list, name='review-list'), 
+    path('reviews/<uuid:review_id>/', views.review_detail, name='review-detail'),
+    path('reviews/create/', views.create_review, name='review-create'),
+    path('reviews/<uuid:review_id>/update/', views.update_review, name='review-update'),
+    path('reviews/<uuid:review_id>/delete/', views.delete_review, name='review-delete'),
+    # This endpoint url pointd for employee CRUD operations
+    path('employees/', views.employee_list, name='employee-list'),
+    path('employees/<uuid:employee_id>/', views.employee_detail, name='employee-detail'),
+    path('employees/create/', views.create_employee, name='employee-create'),
+    path('employees/<uuid:employee_id>/update/', views.update_employee, name='employee-update'),
+    path('employees/<uuid:employee_id>/delete/', views.delete_employee, name='employee-delete'),
+   
 
 ]
--- cleaning/myapprest/views.py
+++ cleaning/myapprest/views.py
@@ -22,7 +22,7 @@ def create_company(request):
     serializer = CompanySerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({"message":"Company created Successfull","data":serializer.data},status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['PUT', 'PATCH'])
@@ -31,7 +31,7 @@ def update_company(request, company_id):
     serializer = CompanySerializer(company, data=request.data, partial=True)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data)
+        return Response({"message":"Company Updated Successfully","data":serializer.data})
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['DELETE'])
@@ -39,4 +39,89 @@ def delete_company(request, company_id):
     company = get_object_or_404(Company, pk=company_id)
     company.is_active=False
     company.save()
-    return Response({"message":"Company Deleted Successful"},Status=status.HTTP_200_OK)  
+    serializer = CompanySerializer(company)
+    return Response({"message":"Company Deleted Successful","data":serializer.data},status=status.HTTP_200_OK)  
+
+@api_view(['GET'])
+def review_list(request):
+    reviews = Review.objects.all()
+    serializer = ReviewSerializer(reviews, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def company_reviews(request, company_id):
+    reviews = Review.objects.filter(company__id=company_id)
+    serializer = ReviewSerializer(reviews, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def review_detail(request, review_id):
+    review = get_object_or_404(Review, pk=review_id)
+    serializer = ReviewSerializer(review)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_review(request):
+    serializer = ReviewSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"message":"You Submitted review succesfull","data":serializer.data}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'PATCH'])
+def update_review(request, review_id):
+    review = get_object_or_404(Review, pk=review_id)
+    serializer = ReviewSerializer(review, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"message":"Review updated_succesfull","data":serializer.data})
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete_review(request, review_id):
+    review = get_object_or_404(Review, pk=review_id)
+    review.is_active=False
+    review.save()
+    serializer = ReviewSerializer(review)
+    return Response({"message": "Review deleted successfully.","data":serializer.data}, status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
+def employee_list(request):
+    employees = Employee.objects.filter(is_active=True)
+    serializer = EmployeeSerializer(employees, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def employee_detail(request, employee_id):
+    employee = get_object_or_404(Employee, pk=employee_id)
+    serializer = EmployeeSerializer(employee)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_employee(request):
+    serializer = EmployeeSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"message":"Employee created successfully","data":serializer.data}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'PATCH'])
+def update_employee(request, employee_id):
+    employee = get_object_or_404(Employee, pk=employee_id)
+    serializer = EmployeeSerializer(employee, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"message":"Review updated_succesfull","data":serializer.data})
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete_employee(request, employee_id):
+    employee = get_object_or_404(Employee, pk=employee_id)
+    employee.is_active = False  
+    employee.save()
+    serializer = EmployeeSerializer(employee)
+    return Response({
+        "message": "Employee deleted successfully.",
+        "data": serializer.data
+    }, status=status.HTTP_200_OK)
