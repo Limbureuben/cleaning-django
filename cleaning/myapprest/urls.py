9a8e7fdc29eb4f9ab1d166a44f3b6b235dfb3e23
Limbureuben <limbureuben4@gmail.com>
2025-06-01 16:16:26 +0300

    changes

--- cleaning/myapprest/urls.py
+++ cleaning/myapprest/urls.py
@@ -2,5 +2,5 @@ from django.urls import path # type: ignore
 from .views import *
 
 urlpatterns = [
-    
+    path('api/v1/register/', UserRegistrationAPIView.as_view(), name='user-registration'),
 ]
