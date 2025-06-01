8a6050ef13f30ce5292bc45a7fe32ade5fa4043d
Limbureuben <limbureuben4@gmail.com>
2025-06-01 16:22:42 +0300

    changes

--- cleaning/myapprest/views.py
+++ cleaning/myapprest/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken  # Or your preferred token lib
 from .serializers import UserRegistrationSerializer
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
 
 
 
@@ -31,3 +33,26 @@ class UserRegistrationAPIView(APIView):
             'success': False,
             'errors': serializer.errors
         }, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if not username or not password:
+            return Response({'error': 'Username and password are required.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(username=username, password=password)
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({
+                'token': token.key,
+                'username': user.username,
+                'email': user.email,
+                'is_staff': user.is_staff
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid username or password.'},
+                            status=status.HTTP_401_UNAUTHORIZED)
