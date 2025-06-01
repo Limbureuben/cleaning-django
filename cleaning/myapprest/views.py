da869a25560cdd3b4587a6d4962ee38c213e8bf6
Limbureuben <limbureuben4@gmail.com>
2025-06-01 16:11:37 +0300

    changes

--- cleaning/myapprest/views.py
+++ cleaning/myapprest/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken  # Or your preferred token lib
+from .serializers import UserRegistrationSerializer
 
-# Create your views here.
+
+
+class UserRegistrationAPIView(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'success': True,
+                'message': "User registered successfully.",
+                'user': {
+                    'username': user.username,
+                    'email': user.email,
+                    'is_staff': user.is_staff,
+                },
+                'token': {
+                    'refresh': str(refresh),
+                    'access': str(refresh.access_token),
+                }
+            }, status=status.HTTP_201_CREATED)
+
+        return Response({
+            'success': False,
+            'errors': serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
