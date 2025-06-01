a11116fa76aec450dbbfafd18a8b73361f7607f7
Limbureuben <limbureuben4@gmail.com>
2025-06-01 20:33:44 +0300

    changes

--- cleaning/myapp/schema.py
+++ cleaning/myapp/schema.py
@@ -3,6 +3,7 @@ from .views import *
 
 class Mutation(graphene.ObjectType):
     register_user = RegisterUser.Field()
+    login_user = LoginUser.Field()
 
 class Query(HelloQuery, graphene.ObjectType):
     pass
