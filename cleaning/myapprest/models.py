07ed6c0672c4ba5c1bf405e92a97a9678b916e34
Limbureuben <limbureuben4@gmail.com>
2025-06-02 21:34:16 +0300

    changes

--- cleaning/myapprest/models.py
+++ cleaning/myapprest/models.py
@@ -1,6 +1,8 @@
 from django.db import models # type: ignore
+from django.contrib.auth.models import User
 
 class Organization(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='organizations')
     organization_name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     email = models.EmailField()
