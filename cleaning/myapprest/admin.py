7515663fc1049aeb132ef3ae7b8aff1039c5adc9
Limbureuben <limbureuben4@gmail.com>
2025-06-02 16:29:22 +0300

    changes

--- cleaning/myapprest/admin.py
+++ cleaning/myapprest/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('organization_name', 'location', 'email', 'address', 'service')
+    search_fields = ('organization_name', 'location', 'email')
+admin.site.register(Organization, OrganizationAdmin)
--- cleaning/myapprest/models.py
+++ cleaning/myapprest/models.py
@@ -1,1 +1,8 @@
 from django.db import models # type: ignore
+
+class Organization(models.Model):
+    organization_name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    email = models.EmailField()
+    address = models.CharField(max_length=100)
+    service = models.CharField(max_length=500)
