7ff940e2a3bb2da46526cf91b2a83f5510c4e647
Limbureuben <limbureuben4@gmail.com>
2025-06-02 21:31:36 +0300

    changes

--- cleaning/cleaning/urls.py
+++ cleaning/cleaning/urls.py
@@ -25,5 +25,5 @@ from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
-    path('api/vi/', include('myapprest.urls')),
+    path('api/', include('myapprest.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
